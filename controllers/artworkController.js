598a0e4bf3df9f7fcb58cad5af7440eeb7c53f04
PunkHazard07 <belloayoola20@gmail.com>
2024-10-24 12:30:58 +0100

    new UpdateController

--- controllers/artworkController.js
+++ controllers/artworkController.js
@@ -1,7 +1,7 @@
 const Artwork = require('../models/artwork'); //importing the model for artwork
 
 //creating new artwork
-exports.createArtwork = (req, res) => {
+exports.createArtwork = async (req, res) => {
     //destructing the model
     const { title, description, price, imageUrl, stockQuantity, category } = req.body;
     try {
@@ -14,17 +14,50 @@ exports.createArtwork = (req, res) => {
             stockQuantity,
             category,
         });
-        const savedArtwork = newArtwork.save();
-        res.status(201).json({success: true, data: savedArtwork});
+        newArtwork.save();
+        res.status(201).json({success: true, data: newArtwork});
     } catch (error) {
-        res.status(500).json({success: false, error: error.message});
+        res.status(500).json({message:'Error creating artwork', error});
     };
 }
 
-// //updating artwork
-// exports.update = (req, res) => {
-    
-//     const { id } = req.params;
-// }
+exports.getArtwork = async (req, res) => {
+    try {
+        const artworks = await Artwork.find();
+        res.status(200).json({success: true, data: artworks});
+    } catch (error) {
+        res.status(500).json({message:'Error fetching artworks', error});
+    };
+}
+
+exports.deleteArtwork = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const artwork = await Artwork.findByIdAndDelete(id);
+        if (!artwork) {
+            return res.status(404).json({ success: false, message: 'Artwork not found'});
+        }
+        res.status(200).json({ success: true,  message: 'Artwork deleted successfully'});
+    } catch (error) {
+        res.status(500).json({message: 'Error deleting artwork', error});
+    };
+}
+
+
+//get single artwork by category
+exports.getCategory = async (req, res) => {
+    //extract category
+    const { category } = req.params;
+    try {
+        const artwork = await Artwork.find({ category });
+
+        if (artwork.length === 0) {
+            return res.status(404).json({ success: false, message: 'Artwork not found'});
+        }
+        res.status(200).json({ success: true, data: artwork});
+    } catch (error) {
+        res.status(500).json({message: 'Error fetching artwork', error});
+    };
+}
 
 
